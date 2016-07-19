0b9f29e5bd56312c94c487aad20c6bcd6054a25d
ppang <yucaroll@naver.com>
2016-07-19 22:32:05 +0900

    carthage 세팅 및 메뉴(SlideMenuController) 생성

---
+++ IOSCoilProject/LeftMenuController.swift
@@ -0,0 +1,25 @@
+//
+//  LeftMenuController.swift
+//  IOSCoilProject
+//
+//  Created by 김혜지 on 2016. 7. 19..
+//  Copyright © 2016년 김혜지. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LeftMenuController : UIViewController{
+    
+    var mainViewController: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
---
+++ IOSCoilProject/UIViewController.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController.swift
+//  IOSCoilProject
+//
+//  Created by 김혜지 on 2016. 7. 19..
+//  Copyright © 2016년 김혜지. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController{
+    func setNavigationBarItem() {
+        self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp")!)
+        self.slideMenuController()?.removeLeftGestures()
+        self.slideMenuController()?.removeRightGestures()
+        self.slideMenuController()?.addLeftGestures()
+        self.slideMenuController()?.addRightGestures()
+    }
+    
+    func removeNavigationBarItem() {
+        self.navigationItem.leftBarButtonItem = nil
+        self.navigationItem.rightBarButtonItem = nil
+        self.slideMenuController()?.removeLeftGestures()
+        self.slideMenuController()?.removeRightGestures()
+    }
+}
